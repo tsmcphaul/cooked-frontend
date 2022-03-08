8a3509ac956cb747eb30af9cc699cc2f7637720f
Tammarra McPhaul <shaemc22@gmail.com>
2022-03-07 21:09:13 -0500

    recipe form

--- src/components/recipes/RecipeForm.js
+++ src/components/recipes/RecipeForm.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
 
 class RecipeForm extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            category: "",
+            ingredients: "",
+            instructions: "", 
+            image: "",
+            preptime: "", 
+            cooktime: ""
+
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+
     render() {
         return (
-            <div>
+            <form onSubmit={this.handleSubmit}>
                 <h2>
-                    New Recipe
+                   Add New Recipe
                 </h2>
-            </div>
+                <input type="text"
+                placeholder="Name"
+                value={this.state.name}
+                onChange={this.handleChange} />
+                <input type="submit" />
+            </form>
         );
     }
 }
