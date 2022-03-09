bbc61eed6692e0f784f00eeff83518221e846b66
Tammarra McPhaul <shaemc22@gmail.com>
2022-03-08 20:23:25 -0500

    reducers/thunk-middleware/actions

---
+++ src/reducers/recipeReducer.js
@@ -0,0 +1,15 @@
+export default function recipeReducer (state, action) {
+    
+    switch (action.type) {
+        case "FETCH_RECIPES":
+            return {
+                
+            }
+            
+            break;
+    
+        default:
+            break;
+    }
+    return state;
+}
