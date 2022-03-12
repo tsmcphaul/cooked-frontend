00cff6407ca6cf7d0c70a080856e24c0d75fa3bf
Tammarra McPhaul <shaemc22@gmail.com>
2022-03-12 17:48:39 -0500

    recipe form

--- src/components/recipes/RecipeForm.js
+++ src/components/recipes/RecipeForm.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import CategorySelect from '../categories/CategorySelect';
+import { fetchCategories } from '../../actions/categoryActions'
+import { connect } from 'react-redux'
+import { fetchRecipes } from '../../actions/recipeActions.js'
 
 class RecipeForm extends Component {
 
@@ -6,21 +10,40 @@ class RecipeForm extends Component {
         super(props);
         this.state = {
             name: "",
-            category: "",
+            category_id: "",
             ingredients: "",
             instructions: "", 
             image: "",
             preptime: "", 
-            cooktime: ""
+            cooktime: "",
+            selectedOption:""
+
 
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleCategoryChange = this.handleCategoryChange.bind(this)
     }
     
+    componentDidMount() {
+        this.props.fetchCategories();
+        this.props.fetchRecipes();
+
+    }
     
     
     handleChange = (e) => {
         this.setState({
             [e.target.name]: e.target.value
+        });
+   
+    }
+
+    handleCategoryChange = (e) => {
+       
+        this.setState({
+            [e.target.name]: e.target.value
+            
         });
    
     }
@@ -34,28 +57,47 @@ class RecipeForm extends Component {
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
+                {console.log(this.state)}
                 <h2>
                    Add New Recipe
-                   {console.log(this.state)}
+                 
                 </h2>
-                <input type="text" placeholder="Name" value={this.state.name} onChange={this.handleChange} />
+                <input type="text" placeholder="Name" value={this.state.name} onChange={this.handleChange} name="name"/>
                 <br/>
-                <select name="category" placeholder="Category" multiple={true} value={this.state.categories} onChange={this.handleChange} />
+                {/* <CategorySelect /> */}
+                <select> { this.props.categories.map(cat => 
+                    <option name="category_id" key={cat.id} value={cat.id} selected={cat.id} onChange={this.handleCategoryChange}>{cat.attributes.name} </option> )
+                }</select>
                 <br/>
-                <textarea type="text" rows="4" cols="50" placeholder="Ingredients" value={this.state.ingredients} onChange={this.handleChange} />
+                <textarea name="ingredients"type="text" rows="4" cols="50" placeholder="Ingredients" value={this.state.ingredients} onChange={this.handleChange} />
                 <br/>
-                <input type="text" placeholder="Instructions" value={this.state.instructions} onChange={this.handleChange} />
+                <textarea name="instructions" type="text" rows="4" cols="50" placeholder="Instructions" value={this.state.instructions} onChange={this.handleChange} />
                 <br/>
-                <input type="text" placeholder="Image" value={this.state.image} onChange={this.handleChange} />
+                <input name="image"type="text" placeholder="Image Link" value={this.state.image} onChange={this.handleChange} />
                 <br/>
-                <input type="text" placeholder="Preptime" value={this.state.preptime} onChange={this.handleChange} />
+                <input name="preptime"type="text" placeholder="Prep Time" value={this.state.preptime} onChange={this.handleChange} />
                 <br/>
-                <input type="text" placeholder="Cooktime" value={this.state.cooktime} onChange={this.handleChange} />
+                <input name="cooktime"type="text" placeholder="Cook Time" value={this.state.cooktime} onChange={this.handleChange} />
                 <br/>
                 <input type="submit" />
             </form>
         );
     }
 }
+const mapStateToProps = state => {
+    // console.log(state)
+    return {
+        categories: state.categories,
+        recipes: state.recipes
+    }
+}
 
-export default RecipeForm;
+// const mapDispatchToProps = state => {
+//     // console.log(state)
+//     return {
+//         categories: state.categories,
+//         recipes: state.recipes
+//     }
+// }
+
+export default connect(mapStateToProps, {fetchCategories, fetchRecipes})(RecipeForm);
